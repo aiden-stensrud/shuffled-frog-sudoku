be443af538ed47e84f4e55ede9e511a615fbf983
Summer <summermueller88@gmail.com>
2026-04-26 21:30:15 -0400

    edited main file

--- main.py
+++ main.py
@@ -23,6 +23,36 @@ def read_input():
       board.append(board_row)
    return board, solutions
 
+def SFLA(F, M, Q, N, S, fixed):
+
+   # Initialize F new frogs
+   all_frogs = [Frog(fixed) for _ in range(F)]
+   global_best = None
+
+   for _ in range(S): 
+      memeplexes, global_best = partition_memeplexes(all_frogs, M)
+
+      if global_best.coll == 0:
+         return global_best
+
+      for m in memeplexes:                   # For each memeplex
+         for _ in range(N):                     # For each evolution step
+            sub_meme, local_best, local_worst = select_submemeplex(m, Q)
+
+            # Makes frog better
+            if not improve_submemeplex(sub_meme, global_best, local_best, local_worst):
+               local_worst.random_grid(fixed)
+               local_worst.evaluate()
+            
+            if local_worst.coll == 0:
+               return local_worst
+
+            m.sort(key = lambda Frog: Frog.coll)
+
+      all_frogs = [item for sublist in memeplexes for item in sublist]
+
+   return global_best
+
 
 # Reads input
 fixed, max_solutions = read_input()
@@ -35,20 +65,8 @@ S = 0           # number of times the memeplexes are shuffled
 
 assert S * N * M <= max_solutions
 
-# Initialize F new frogs
-all_frogs = [Frog(fixed) for _ in range(F)]
+# Run the main algorithm
+best_solution = SFLA(F, M, Q, N, S, fixed)
 
-for _ in range(S): 
-   # SFLA
-   memeplexes, global_best = partition_memeplexes(all_frogs)
-   for m in memeplexes:                   # For each memeplex
-      for _ in range(N):                     # For each evolution step
-         sub_meme, local_best, local_worst = select_submemeplex(m, Q)
-
-         # Makes frog better
-         if not improve_submemeplex(sub_meme, global_best, local_best, local_worst):
-            local_worst.random_grid(fixed)
-
-   all_frogs = [item for sublist in memeplexes for item in sublist]
-
-
+# Prints the output
+best_solution.print_board()
